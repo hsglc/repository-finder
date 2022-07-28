aa5ca599eba7657fba091d059e4f42de1bcb287c
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-28 19:09:45 +0300

    feat: Create a header component.

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-blue-600 text-white py-3 px-1.5 md:p-6'>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-2xl font-bold md:pl-5 tracking-wider'>GITHUB REPOSITORY FINDER</h1>
+        <img src="/img/github-logo.png" alt="github-logo" />
+      </div>
+    </header>
+  )
+}
+
+export default Header
