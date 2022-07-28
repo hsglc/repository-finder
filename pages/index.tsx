816085ec19a7cdf22674eaa5b6922f129cc05704
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-28 19:14:35 +0300

    feat: Implement component the main page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,32 @@
-import type { NextPage } from 'next'
+import Table from '../components/RepositoryTable/Table'
+import Pagination from '../components/Pagination'
+import SearchBox from '../components/SearchBox'
+import Header from '../components/Header'
+import RepositoryCard from '../components/RepositoryCard'
+import { useSelector } from 'react-redux'
 import Head from 'next/head'
-import Image from 'next/image'
 
-const Home: NextPage = () => {
+const Home = () => {
+  const repoNumber = useSelector((state: any) => state.repository.repoNumber)
+  
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className='flex flex-col'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Github Repository Finder</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <Header />
+      <main className='centered'>
+        <SearchBox />
+        <p className='font-semibold -mb-4 mt-2 ml-1.5 text-gray-400'>Showing <span className='text-gray-700'>{repoNumber}</span> repositories</p>
+        <Table />
+        <RepositoryCard />
+        <Pagination itemsPerPage={5} items={repoNumber}  />
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
+
   )
 }
 
 export default Home
+
