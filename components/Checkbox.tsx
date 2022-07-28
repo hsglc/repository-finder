4b8f378b02fb29395dcb56f8acd18f742d279904
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-28 22:45:29 +0300

    feat: Implemennt typed hooks.

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,8 +1,8 @@
-import { SyntheticEvent } from 'react';
+
 import { useSelector } from 'react-redux'
-import { useDispatch } from "react-redux";
 import { fetchRepositories } from "../store/repository-actions";
 import { repositoryActions } from "../store/repository-slice";
+import {useAppDispatch} from '../hooks/types'
 
 interface IProps {
     label: string;
@@ -10,14 +10,15 @@ interface IProps {
 }
 
 const Checkbox = ({ label, value }: IProps) => {
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
     const selectedLanguage = useSelector((state: any) => state.repository.selectedLanguage);
     const search = useSelector((state: any) => state.repository.search);
     
     return (
-        <div onChange={(e: SyntheticEvent) => {
-            dispatch(repositoryActions.setSelectedLanguage(e.target.value))
-            dispatch(fetchRepositories(e.target.value, 1, search))
+        <div onChange={(e) => {
+            const result = (e.target as HTMLInputElement).value;
+            dispatch(repositoryActions.setSelectedLanguage(result))
+            dispatch(fetchRepositories(result, 1, search))
             
         }} className="flex items-center" >
             <input checked={selectedLanguage === value}  id="default-radio-2" type="radio" value={value} name="default-radio" className="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 " />
--- store/repository-slice.tsx
+++ store/repository-slice.tsx
@@ -1,6 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
-const initialState = {
+interface IRepositoryState {
+    repositories: any[];
+    repoNumber: number;
+    selectedLanguage: string;
+    search: string;
+    page: number;
+}
+
+const initialState: IRepositoryState = {
     repositories: [],
     page: 1,
     selectedLanguage: 'Javascript',
@@ -12,20 +21,20 @@ const repositorySlice = createSlice({
     name: "repository",
     initialState,
     reducers: {
-        setRepositories: (state, action) => {
+        setRepositories: (state, action:PayloadAction<any[]>) => {
             state.repositories = action.payload;
 
         },
-        setPage: (state, action) => {
+        setPage: (state, action:PayloadAction<number> ) => {
             state.page = action.payload;
         },
-        setSelectedLanguage: (state, action) => {
+        setSelectedLanguage: (state, action: PayloadAction<string>) => {
             state.selectedLanguage = action.payload;
         },
-        setSearch: (state, action) => {
+        setSearch: (state, action: PayloadAction<string>) => {
             state.search = action.payload;
         },
-        setRepoNumber: (state, action) => {
+        setRepoNumber: (state, action: PayloadAction<number>) => {
             state.repoNumber = action.payload;
         }
     },
