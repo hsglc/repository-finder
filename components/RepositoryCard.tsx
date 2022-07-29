3e9e8ba80fc486325288ed8b4b7e0afc08baa2a2
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-29 20:33:58 +0300

    refactor: Change font weight of id col.

--- components/RepositoryCard.tsx
+++ components/RepositoryCard.tsx
@@ -7,7 +7,7 @@ const RepositoryCard = () => {
   return (
     <div className="block md:hidden mt-4">
       {repositories?.map((repository: any) => <div key={repository.id} className="border rounded-lg border-gray-400/50 p-2 mb-2 mx-1.5">
-        <h1> <span className="repCardTitle">ID: </span> {repository.id}</h1>
+        <h1 className="font-bold"> <span className="repCardTitle">ID: </span> {repository.id}</h1>
         <p> <span className="repCardTitle">Owner: </span> {repository.name}</p>
         <p className="truncate..."> <span className="repCardTitle">Description: </span> {repository.description?.substring(0, 100)}</p>
         <p> <span className="repCardTitle">Stars: </span> {repository.stars}</p>
