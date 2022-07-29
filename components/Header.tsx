d891ced3b62896e118c4615b8b77d746d57d3ca3
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-29 20:33:21 +0300

    refactor: Decrease h1 tag size.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className='bg-blue-600 text-white py-3 px-1.5 md:p-6'>
       <div className='flex items-center justify-between'>
-        <h1 className='text-2xl font-bold md:pl-5 tracking-wider'>GITHUB REPOSITORY FINDER</h1>
+        <h1 className='text-lg md:text-2xl font-bold md:pl-5 tracking-wider'>GITHUB REPOSITORY FINDER</h1>
         <img src="/img/github-logo.png" alt="github-logo" />
       </div>
     </header>
