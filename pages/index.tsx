91bc1505d28616a23e2c178fe6318eefd40230f4
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-29 20:33:34 +0300

    refactor: Remove some lines.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,10 @@ import Pagination from '../components/Pagination'
 import SearchBox from '../components/SearchBox'
 import Header from '../components/Header'
 import RepositoryCard from '../components/RepositoryCard'
-import { useSelector } from 'react-redux'
 import Head from 'next/head'
 
 const Home = () => {
-  const repoNumber = useSelector((state: any) => state.repository.repoNumber)
-  
+
   return (
     <div className='flex flex-col'>
       <Head>
@@ -18,10 +16,9 @@ const Home = () => {
       <Header />
       <main className='centered'>
         <SearchBox />
-        <p className='font-semibold -mb-4 mt-2 ml-1.5 text-gray-400'>Showing <span className='text-gray-700'>{repoNumber}</span> repositories</p>
         <Table />
         <RepositoryCard />
-        <Pagination itemsPerPage={5} items={repoNumber}  />
+        <Pagination itemsPerPage={5} />
       </main>
     </div>
 
