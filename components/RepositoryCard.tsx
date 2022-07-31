17114ee5c0dfca25abb912cf6a5f354bea64e225
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-31 15:12:00 +0300

    refactor: Get data from custom hook and replace div element with section.

--- components/RepositoryCard.tsx
+++ components/RepositoryCard.tsx
@@ -1,11 +1,10 @@
-import { useSelector } from "react-redux"
-import { IRepository } from "../types"
+import useStoreSlice from "../hooks/useStoreSlice"
 import CustomLink from "./CustomLink"
 
 const RepositoryCard = () => {
-  const repositories = useSelector((state: IRepository) => state.repository.repositories)
+  const { repositories } = useStoreSlice()
   return (
-    <div className="block md:hidden mt-4">
+    <section className="block md:hidden mt-4">
       {repositories?.map((repository: any) => <div key={repository.id} className="border rounded-lg border-gray-400/50 p-2 mb-2 mx-1.5">
         <h1 className="font-bold"> <span className="repCardTitle">ID: </span> {repository.id}</h1>
         <p> <span className="repCardTitle">Owner: </span> {repository.name}</p>
@@ -16,7 +15,7 @@ const RepositoryCard = () => {
         <p> <span className="repCardTitle">View More: </span> <CustomLink href={repository.link} label="Github" /> </p>
 
       </div>)}
-    </div>
+    </section>
   )
 }
 
