e059ce1724d49d54ec70b956760ad1296ca83fa4
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-31 15:09:40 +0300

    feat: Create a spinner component to show at while data fetching

---
+++ components/Spinner.tsx
@@ -0,0 +1,23 @@
+import { RotatingLines } from 'react-loader-spinner'
+
+interface IProps {
+    message: string
+
+}
+
+const Spinner = ({ message }: IProps) => {
+    return (
+        <div className='mt-12 flex flex-col items-center justify-center'>
+            <RotatingLines
+                strokeColor="grey"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="96"
+
+            />
+            <p className='text-blue-600 font-semibold text-2xl'>{message}</p>
+        </div>
+    )
+}
+
+export default Spinner
