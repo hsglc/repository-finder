573aacdfee20f8b982b0a010cc1fa819ae16c3b5
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-31 09:07:10 +0300

    refactor: Save previous page state and use it after user change the selected language.

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -13,14 +13,13 @@ const Checkbox = ({ label, value }: IProps) => {
     const dispatch = useAppDispatch();
     const selectedLanguage = useSelector((state: any) => state.repository.selectedLanguage);
     const search = useSelector((state: any) => state.repository.search);
-
+    const page = useSelector((state: any) => state.repository.page);
 
     return (
         <div onChange={(e) => {
             const result = (e.target as HTMLInputElement).value;
             dispatch(repositoryActions.setSelectedLanguage(result))
-            // dispatch(fetchRepositories(result, 1, search))
-            dispatch(fetchRepos({ page: 1, selectedLang: result, search }))
+            dispatch(fetchRepos({ page, selectedLang: result, search }))
 
         }} className="flex items-center" >
             <input readOnly checked={selectedLanguage === value} id="default-radio-2" type="radio" value={value} name="default-radio" className="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 " />
