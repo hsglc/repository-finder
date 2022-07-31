d0d2ca14cec174f82d6a5b13170e8fb772532183
huseyin gulcu <huseyingulcuu@gmail.com>
2022-07-31 15:11:17 +0300

    refactor: Get data from custom hook and make some style changes.

--- components/RepositoryTable/Table.tsx
+++ components/RepositoryTable/Table.tsx
@@ -1,22 +1,21 @@
 import { IRepository } from "../../types";
-import { useSelector } from "react-redux";
+import useStoreSlice from "../../hooks/useStoreSlice";
 import Thead from "./Thead";
 import Trow from "./Trow";
 
 
 const RepositoryTable = () => {
-  const repositories = useSelector((state: any) => state.repository.repositories);
-  const error = useSelector((state: any) => state.repository.error);
+  const { repositories } = useStoreSlice()
 
   return (<>
-    {!error ? <table className="hidden md:inline-block my-8 shadow-lg border-separate border-spacing-0  border border-gray-400/50 rounded-lg p-4">
+    <table className="hidden md:inline-block my-1 md:my-4 shadow-lg border-separate border-spacing-0  border border-gray-400/50 rounded-lg p-4">
       <Thead />
       <tbody>
         {repositories?.map((repository: IRepository) => (
           <Trow key={repository.id} repository={repository} />
         ))}
       </tbody>
-    </table> : <div className="text-center mt-4 text-red-400 font-bold">{error}</div>}
+    </table>
   </>
   );
 };
