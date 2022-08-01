239699f8131e23e6135f467474340409d5398853
huseyin gulcu <huseyingulcuu@gmail.com>
2022-08-01 10:43:05 +0300

    feat: Add a linear gradient to header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
   return (
-    <header className='bg-blue-600 text-white py-3 px-1.5 md:p-6'>
+    <header className='bg-gradient-to-r from-indigo-500 via-blue-400 to-blue-300 text-white py-3 px-1.5 md:p-6'>
       <div className='flex items-center justify-between'>
         <h1 className='text-lg md:text-2xl font-bold md:pl-5 tracking-wider'>GITHUB REPOSITORY FINDER</h1>
         <img src="/img/github-logo.png" alt="github-logo" />
